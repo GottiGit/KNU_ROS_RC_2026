82fa1de38954fad04f5a57d0adb10d3fe61840c7
freshmea <freshmea@gmail.com>
2026-06-23 10:41:58 +0900

    index.html

--- python/a17_datetime2.py
+++ python/a17_datetime2.py
@@ -1,7 +1,10 @@
 import datetime
 
+from a01_hello import main as hello_main
+
 
 def main():
+    hello_main()
     now = datetime.datetime.now()
 
     if 9 < now.hour < 12:
@@ -17,6 +20,26 @@ def main():
     # 4, 5 -> 봄
     # 6, 7, 8 -> 여름
     # 9, 10, 11 -> 가을
+    if now.month < 3:
+        print(f"이번 달은 {now.month}로 겨울입니다.")
+    elif now.month <= 5:
+        print(f"이번 달은 {now.month}로 봄입니다.")
+    elif now.month <= 8:
+        print(f"이번 달은 {now.month}로 여름입니다.")
+    elif now.month <= 11:
+        print(f"이번 달은 {now.month}로 가을입니다.")
+    else:
+        print(f"이번 달은 {now.month}로 겨울입니다.")
+
+    if now.month in [12, 1, 2, 3]:
+        print("겨울")
+    elif now.month in [4, 5]:
+        print("봄")
+    elif now.month in [6, 7, 8]:
+        print("여름")
+    elif now.month in [9, 10, 11]:
+        print("가을")
+
 
 if __name__ == "__main__":
     main()
