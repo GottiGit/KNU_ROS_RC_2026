168d9c90616d205a24f528372d2a8aa85ffa071c
freshmea <freshmea@gmail.com>
2026-06-25 10:29:25 +0900

    ctreate student

--- python/a62_class_make_student.py
+++ python/a62_class_make_student.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 
 
@@ -13,9 +12,17 @@ def create_student(name, korean, math, english, science):
 
 
 def main():
-    student_path = Path(__file__).parent.parent / "data/student_info.json"
+    student_path = Path(__file__).parent.parent / "data/student_info.data"
+    students = []
     with open(student_path, "r", encoding='utf-8') as f:
-        students = json.load(f)
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            name, korean, math, english, science = line.split()
+
+            student = create_student(name, int(korean), int(math), int(english), int(science))
+            students.append(student)
 
     print("이름\t총점\t평균")
     for student in students:
