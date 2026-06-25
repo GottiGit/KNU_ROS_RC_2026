07f40564cc390a8e337c217a138d5d3a07518d3b
freshmea <freshmea@gmail.com>
2026-06-25 14:50:12 +0900

    class method

--- python/a67_class_variable.py
+++ python/a67_class_variable.py
@@ -2,14 +2,22 @@ from pathlib import Path
 
 
 class Student:
+    count = int()
+
     def __init__(self, name, korean, math, english, science):
+        # 인스턴스 변수는 init 에서 생성한다
+        # (규칙이 아니지 왠만하면 여기서 만드시오)
+        # init 은 생성할때 반드시 한번만 실행 됩니다
         self.name = name
         self.korean = korean
         self.math = math
         self.english = english
         self.science = science
+        Student.count += 1
+        print(f"{Student.count} 번째 학생이 생성 되었습니다.")
 
     def get_sum(self):
+        self.sum = "합계입니다"
         return self.korean + self. math + self.english + self.science
 
     def get_average(self):
@@ -55,7 +63,7 @@ class Student:
             raise ValueError
 
 
-def main():
+def load_student_data():
     student_path = Path(__file__).parent.parent / "data/student_info.data"
     students = []
     with open(student_path, "r", encoding='utf-8') as f:
@@ -67,18 +75,19 @@ def main():
 
             student = Student(name, int(korean), int(math), int(english), int(science))
             students.append(student)
+    return students
 
+
+def main():
+    students = load_student_data()
+
+    print(f"현재 생성된 총 학생수는 {Student.count} 입니다")
+    # # 클래스 변수를 쓸 때 객체에 접근해서 쓸 수 있다 하지만 쓰지말자.
+    # print(f"현재 생성된 총 학생수는 {students[0].count} 입니다")
     print("이름\t총점\t평균")
     for student in students:
         print(student)
-
-    if students[5] < students[7]:
-        print("같다")
-    else:
-        print("다르다")
-
-    print(isinstance(Student("a", 1, 2, 3, 4), object))
-    # 모든 파이썬의 부모 클래스는 object 이다.
+    print(students[0].sum)
 
 
 if __name__ == "__main__":
--- python/a68_class_method.py
+++ python/a68_class_method.py
@@ -2,18 +2,38 @@ from pathlib import Path
 
 
 class Student:
+    count = int()
+    students = []
+
     def __init__(self, name, korean, math, english, science):
+        # 인스턴스 변수는 init 에서 생성한다
+        # (규칙이 아니지 왠만하면 여기서 만드시오)
+        # init 은 생성할때 반드시 한번만 실행 됩니다
         self.name = name
         self.korean = korean
         self.math = math
         self.english = english
         self.science = science
+        Student.count += 1
+        Student.students.append(self)
+        print(f"{Student.count} 번째 학생이 생성 되었습니다.")
 
     def get_sum(self):
+        self.sum = "합계입니다"
         return self.korean + self. math + self.english + self.science
 
     def get_average(self):
         return self.get_sum() / 4
+
+    @classmethod
+    def print(cls):
+        print(f"현재 생성된 총 학생수는 {Student.count} 입니다")
+        print()
+        print("---------------학생목록---------------")
+        print("이름\t총점\t평균")
+        for student in cls.students:
+            print(student)
+        print("--------------------------------------")
 
     def __str__(self):
         return f"{self.name}\t{self.get_sum()}\t{self.get_average()}"
@@ -55,9 +75,8 @@ class Student:
             raise ValueError
 
 
-def main():
+def load_student_data():
     student_path = Path(__file__).parent.parent / "data/student_info.data"
-    students = []
     with open(student_path, "r", encoding='utf-8') as f:
         for line in f:
             line = line.strip()
@@ -65,20 +84,12 @@ def main():
                 continue
             name, korean, math, english, science = line.split()
 
-            student = Student(name, int(korean), int(math), int(english), int(science))
-            students.append(student)
+            Student(name, int(korean), int(math), int(english), int(science))
 
-    print("이름\t총점\t평균")
-    for student in students:
-        print(student)
 
-    if students[5] < students[7]:
-        print("같다")
-    else:
-        print("다르다")
-
-    print(isinstance(Student("a", 1, 2, 3, 4), object))
-    # 모든 파이썬의 부모 클래스는 object 이다.
+def main():
+    load_student_data()
+    Student.print()
 
 
 if __name__ == "__main__":
